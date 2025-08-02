0131f014308e3c01a4de9f7831afa0f560395bd4
MaccSob <71sobmaciej@gmail.com>
2025-08-02 23:55:05 +0200

    deleted useless, added new

---
+++ mysite/src/app/about/aboutpage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+const About = () => {
+
+    return <>
+    
+    PL/ENG ABOUT ME HERE 
+
+    
+    
+    </>
+}
+export default About;
---
+++ mysite/src/app/contact/contactpage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+const Contact = () => {
+
+    return <>
+    
+   CONTACT HERE PL/ENG
+    
+    
+    
+    </>
+}
+export default Contact;
