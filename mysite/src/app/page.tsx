4cfc1d18b6ca1903e41cb893c083031ae214c342
MaccSob <71sobmaciej@gmail.com>
2025-08-09 23:51:53 +0200

    layout and which projects on homepage

--- mysite/src/app/page.tsx
+++ mysite/src/app/page.tsx
@@ -1,9 +1,31 @@
-
+import '../app/global.css';
 
 export default function Home() {
   return (
+    <>
     <h1>
-      There will be a page!
+     Software Developer Portfolio
     </h1>
+    <p className='undertext'>Personal projects that I've worked so far:</p>
+
+    <div className="container">
+
+      <div className="gridbox">1</div>
+      <div className="gridbox">2</div>
+      <div className="gridbox">3</div>
+      <div className="gridbox">4</div>
+      <div className="gridbox">5</div>
+      <div className="gridbox">6</div>
+
+
+
+
+
+
+    </div>
+
+
+
+    </>
   );
 }
